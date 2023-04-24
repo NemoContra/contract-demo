7655e0d87a49845ad96757f830e5a3ac57f73a3a
Marco Haemmerle <m.haemmerle@nemocontra.de>
2023-04-24 10:17:40 +0200

    update all dependencies

--- apps/contract-frontend/src/app/app.component.spec.ts
+++ apps/contract-frontend/src/app/app.component.spec.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientModule],
+      imports: [RouterTestingModule],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
